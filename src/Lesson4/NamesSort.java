d6152a750c207233f2ab1dd3a5e221fa1985f943
tempanas <krysukhalet@gmail.com>
2023-07-27 21:42:57 +0300

    HW 4 Names sort

---
+++ src/Lesson4/NamesSort.java
@@ -0,0 +1,11 @@
+package Lesson4;
+
+import java.util.Arrays;
+
+public class NamesSort {
+    public static void main(String[] args) {
+        String str[] = {"Kate", "Alex", "Iisak", "Jesus", "Jane", "Max", "Pavel", "Sophy"};
+        Arrays.sort(str);
+        System.out.println(Arrays.toString(str));
+    }
+}
